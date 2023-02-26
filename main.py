ce70862493632cd145e8e1cb42b83919c8c9b5dd
Amirka123123 <yakubovamirbek098@gmail.com>
2023-02-26 23:40:17 +0500

    1. Add main menu 2. I add def of welcome

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("Hi there! What's up?")
-def menu_search()
+import sqlite3
+
+from telebot import TeleBot
+from telebot.types import KeyboardButton, ReplyKeyboardMarkup
+
+TOKEN = "6290184537:AAHsxexwzfKz95Y_vSd51KHGIuwE9zlRVoU"
+
+bot = TeleBot(TOKEN, parse_mode="MARKDOWN")
+
+def main_menu(): #первоначальная клавиатура
+    search_button = KeyboardButton("Search🔎")
+    kitchen_button = KeyboardButton("Foreign kitchens🍜")
+
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(search_button)
+    keyboard.add(kitchen_button)
+
+    return keyboard
+
+
+@bot.message_handler(func=lambda message: message.text == "/start")
+def welcome(message):
+    reply = "Hi there! This bot will help you to find the ingredients for foreign meals."
+    bot.reply_to(message, reply, reply_markup=main_menu())
+
+
+bot.infinity_polling()
